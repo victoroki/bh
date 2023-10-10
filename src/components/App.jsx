d8f0c341a546370a6c0664785b39f8809ff23a11
victoroki <okiomeriv@gmail.com>
2023-10-10 22:10:28 +0300

    remove err

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './Header';
-import Hero from './Hero';
 import Inspiration from './Inspiration';
 import Discover from './Discover';
 import Hosting from './Hosting';
